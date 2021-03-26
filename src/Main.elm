88c78d81bc100157ae8c97163ba9c16c9ebf5fa2
Grimmbol <krogstie.oystein@gmail.com>
2021-03-26 14:34:34 +0100

    add more visual elements

--- src/Main.elm
+++ src/Main.elm
@@ -1,17 +1,11 @@
 module Main exposing (..)
 
-import Browser
-import Html exposing (..)
+import Html exposing ( Html, div, text, h1, h2, a )
 import Html.Attributes exposing ( class, href )
-import DividerLine exposing (..)
-import SpinLoader exposing (..)
-import Http exposing (..)
-import Json.Decode exposing (Decoder, field, string, list)
+import DividerLine exposing ( dividerLine, dividerLineShort )
+import Browser
 
-import Random
-
-
----- MODEL ----
+---- TESTDATA ----
 subHeaderList =
     [ "Bare ræl"
     , "Også på Skedsmo"
@@ -22,138 +16,119 @@ subHeaderList =
     , "aaaaaaaa"
     ]
 
- 
+dummyHaiku =
+    ["test test mange test"
+    ,"mange sauer på en gang"
+    ,"brølende breking"]
 
+dummyArticle1 =
+    { title = "test1"
+    , ingress = "spennende med kniv!"
+    , image = "image1.url"
+    }
+
+dummyArticle2 =
+    { title = "Kålprisene stiger igjen"
+    , ingress = "Hva med potet?"
+    , image = "image2.url"
+    }
+
+dummyArticle3 =
+    { title = "17 store epler på en gang"
+    , ingress = "Du trenger et trau"
+    , image = "image3.url"
+    }
+
+dummyArticles =
+    [ dummyArticle1
+    , dummyArticle2
+    , dummyArticle3
+    ]
+    
+---- MODEL ----
 urlBase = "https://mjukvare-no-api.herokuapp.com/"
 
+type alias Haiku = List String
 type alias HeaderText = String
-type alias State = {haiku : (List String), subheader : HeaderText}
-
-type Model 
-    = Failure
-    | LoadingHeader
-    | LoadingHaiku HeaderText
+type alias ArticleInfo =
+    { title: String
+    , ingress: String 
+    , image: String
+    }
+          
+type alias State =
+    { haiku: Haiku
+    , subHeader: HeaderText
+    , articles: List ArticleInfo
+    }
+     
+type Model
+    = Failiure
+    | Loading State
     | Success State
 
 init : () -> (Model, Cmd Msg)
-init _  = 
-    ( LoadingHeader, getRandomHeader)
+init _ =
+    (Failiure, Cmd.none)
 
 
+---- Update ----
+type Msg = Nei
 
----- UPDATE ----
 
-type Msg
-    = GetSubHeader
-    | GotSubHeaderIndex Int
-    | GetHaiku
-    | GotHaiku (Result Http.Error (List String))
-
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        GetSubHeader ->
-            (model, getRandomHeader)
-                
-        GotSubHeaderIndex newSubHeaderIndex -> 
-            update GetHaiku
-                (LoadingHaiku
-                     (getElementFromList subHeaderList newSubHeaderIndex))
-            
-        GetHaiku -> 
-            (model, getHaiku)
+        Nei -> 
+            (Failiure, Cmd.none)
 
-        GotHaiku result -> 
-            case result of 
-                Ok newHaiku ->
-                    case model of
-                        LoadingHaiku newHeader ->
-                            (Success {haiku = newHaiku, subheader = newHeader}, Cmd.none)
-                        _ ->
-                            (Success {haiku = newHaiku, subheader = "no header!"}, Cmd.none)
-                Err _ -> 
-                    (Failure, Cmd.none)
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
----- VIEW ----
+
 
 view : Model -> Html Msg
 view model =
     div [] [
-         headerView model
-        ,haikuView model
-    ]
+         div [class "content-wrapper"] [
+              div [class "content-column"] [
+                   headerView model
+                  , dividerLine
+                  , haikuView model
+                  , dividerLine
+                  , articlesView model
+                  ]
+             ]
+        ]
 
 headerView : Model -> Html Msg
 headerView model =
-    case model of
-        LoadingHeader ->
-            div [ class "title" ]
-                [ h1 [ class "title-top" ] [ text "Mjukvarelauget" ]
-                , dividerLine
-                , h2 [ class "title-bottom"] [ text "loading ..." ]
-                ]
-        
-        LoadingHaiku headerText ->
-            div [ class "title" ]
-                [ h1 [ class "title-top" ] [ text "Mjukvarelauget" ]
-                , dividerLine
-                , h2 [ class "title-bottom"] [ text headerText ]
-                ]
+    div [ class "title" ]
+        [ h1 [ class "title-top" ] [ text "Mjukvarelauget" ]
+        , dividerLineShort
+        , h2 [ class "title-bottom"] [ text "Bare ræl" ]
+        ]
 
-        Success state ->
-            div [ class "title" ]
-                [ h1 [ class "title-top" ] [ text "Mjukvarelauget" ]
-                , dividerLine
-                , h2 [ class "title-bottom"] [ text state.subheader ]
-                ]
-        Failure ->
-            div [ class "title" ]
-                [ h1 [ class "title-top" ] [ text "Mjukvarelauget" ]
-                , dividerLine
-                , h2 [ class "title-bottom"] [ text "Failed to load" ]
-                ]
-            
 haikuView : Model -> Html Msg
 haikuView model =
-    div [] [
-        viewHaiku model 
-    ]
+    div [ class "haiku-wrapper" ] [
+         renderList dummyHaiku
+        ,h2 [class "what"] [
+              a [ href "https://github.com/mjukvarelauget/badhaiku" ] [ text "Hæ?" ] 
+             ]
+        ]
 
-viewHaiku : Model -> Html Msg
-viewHaiku model =
-    case model of 
-        Failure -> 
-            div [class "haiku-wrapper"] [ 
-                 h2 [ class "haiku" ] [text "No Haiku for you"] 
-                ]
-
-        LoadingHeader ->
-            div [class "haiku-wrapper"] [ 
-                 h2 [ class "haiku" ] [text "Loading..."] 
-                ]
-
-        LoadingHaiku _ ->
-            div [class "haiku-wrapper"] [ 
-                 h2 [ class "haiku" ] [text "Loading..."] 
-                ]
-
-        Success state -> 
-            div [ class "haiku-wrapper" ] [
-                renderList state.haiku
-                ,h2 [class "what"] [
-                    a [ href "https://github.com/mjukvarelauget/badhaiku" ] [ text "Hæ?" ] 
-                ]
-            ]
-
+articlesView : Model -> Html Msg
+articlesView model = div [class "articles-wrapper" ] [
+                     
+                      ]
 renderList : List String -> Html Msg
 renderList lst =
     div []
         (List.map (\l -> h2 [] [ text l ]) lst)
+            
 ---- PROGRAM ----
-
 main : Program () Model Msg
 main =
     Browser.element
@@ -162,38 +137,3 @@ main =
         , subscriptions = subscriptions
         , view = view
         }
-
-
--- HTTP
-getHaiku : Cmd Msg
-getHaiku = 
-    Http.get
-        { 
-          url = urlBase ++ "/bad/haiku"
-        , expect = Http.expectJson GotHaiku haikuDecoder
-        }
-
-haikuDecoder : Decoder (List String)
-haikuDecoder = 
-    field "haiku" (list string)
-
--- Random
-getRandomHeader : Cmd Msg
-getRandomHeader =
-    Random.generate GotSubHeaderIndex (Random.int 0 6)
-
--- Utility
-getElementFromList : (List String) -> Int -> String
-getElementFromList list index =
-    case list of
-        (head::tail) ->
-            if index < 0 then
-                "Invalid index (negative)"
-            else
-                if index == 0 then
-                    head
-                else
-                    getElementFromList tail (index - 1)
-
-        [] ->
-            "Empty list!"
